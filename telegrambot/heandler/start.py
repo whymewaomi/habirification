325dab9411a752cabb3a9c5a69c5561a26462ec4
whymewaomi <whymewaomi@icloud.com>
2025-11-10 20:34:58 +0300

    up

--- telegrambot/heandler/start.py
+++ telegrambot/heandler/start.py
@@ -2,13 +2,11 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 
-import keyboard.keyboard as kb
+import telegrambot.keyboard.keyboard as kb
 
 startbot = Router()
 
-startbot.message(Command("start"))
+@startbot.message(Command("start"))
 async def startanswer(message: Message):
-    await message.answer("Привет! \nДля того чтобы пользоваться ботом нужно",
-                         "\nзарегистрироваться или войти в",
-                         "\nсвой аккаунт", reply_markup=kb.start())
+    await message.answer("Привет! \nДля того чтобы пользоваться ботом нужно \nзарегистрироваться или войти в \nсвой аккаунт", reply_markup=kb.start())
     
--- telegrambot/main.py
+++ telegrambot/main.py
@@ -4,18 +4,24 @@ from aiogram import Dispatcher, Bot
 from telegrambot.config import TOKEN
 from telegrambot.heandler.start import startbot
 
-dp = Dispatcher()
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+# Создаём объекты бота и диспетчера
 bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
+async def main():
+    # Подключаем роутеры
+    dp.include_router(startbot)
 
-async def main_polling():
-    dp.include_routers(startbot)
-    dp.include_router()
+    print("🚀 Бот запущен!")
+    # Запускаем процесс polling
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-        try:
-            asyncio.run(main_polling())
-        except (KeyboardInterrupt, SystemExit):
-            logging.error("Бот выключен!")
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("🛑 Бот остановлен!")
