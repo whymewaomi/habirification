fdb3388d0cd1c4536a4b675cc1b27f8a6aec136e
whymewaomi <whymewaomi@icloud.com>
2025-11-10 20:35:04 +0300

    up

--- telegrambot/keyboard/keyboard.py
+++ telegrambot/keyboard/keyboard.py
@@ -5,6 +5,5 @@ def start():
     menu = InlineKeyboardBuilder()
     menu.button(text="Войти", callback_data="login")
     menu.button(text="Зарегистрироваться", callback_data="reg")
-    menu.adjust(1, 1)
-    menu.as_markup()
-    return menu
+    menu.adjust(2, 1)
+    return menu.as_markup()
